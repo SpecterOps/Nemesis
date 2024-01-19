7f34ffd7621b0a6ec813cac19edbd863bee31c13
Will <HarmJ0y@users.noreply.github.com>
2024-01-19 12:47:34 -0800

    Changed embedding model for NLP container
    -Changed embedding model for NLP container to one that has a proper context window
    -Updated to ElasticsearchStore in NLP app
    -No longer collapse all whitespace in indexing

--- cmd/nlp/nlp/services/indexing.py
+++ cmd/nlp/nlp/services/indexing.py
@@ -8,8 +8,9 @@ import uuid
 import nemesispb.nemesis_pb2 as pb
 import structlog
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import ElasticVectorSearch
+from langchain.text_splitter import (RecursiveCharacterTextSplitter,
+                                     TokenTextSplitter)
+from langchain_community.vectorstores.elasticsearch import ElasticsearchStore
 from nemesiscommon.messaging import MessageQueueConsumerInterface
 from nemesiscommon.messaging_rabbitmq import SingleQueueRabbitMQWorker
 from nemesiscommon.storage import StorageInterface
@@ -24,7 +25,7 @@ class IndexingService(SingleQueueRabbitMQWorker):
     cfg: NLPSettings
     storage: StorageInterface
     embeddings: HuggingFaceEmbeddings
-    vector_store: ElasticVectorSearch
+    vector_store: ElasticsearchStore
     text_splitter: RecursiveCharacterTextSplitter
 
     def __init__(self, inputQ: MessageQueueConsumerInterface, cfg: NLPSettings, storage: StorageInterface) -> None:
@@ -33,11 +34,30 @@ class IndexingService(SingleQueueRabbitMQWorker):
         self.storage = storage
         self.cfg = cfg
 
+        chunk_size = int(self.cfg.text_chunk_size)
+        chunk_overlap = int(chunk_size/15)
+        logger.info(f"indexing model: {self.cfg.embedding_model}, text chunk_size: {chunk_size}, chunk_overlap: {chunk_overlap}")
+
         self.embeddings = HuggingFaceEmbeddings(model_name=self.cfg.embedding_model)
-        self.vector_store = ElasticVectorSearch(embedding=self.embeddings, elasticsearch_url=self.cfg.elastic_connection_uri, index_name=self.cfg.elastic_index_name)
+
+        self.vector_store = ElasticsearchStore(
+            es_url=self.cfg.elasticsearch_url,
+            es_user=self.cfg.elasticsearch_user,
+            es_password=self.cfg.elasticsearch_password,
+            index_name=self.cfg.elastic_index_name,
+            embedding=self.embeddings
+        )
 
         # significantly faster than HuggingFace tokenization
-        self.text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=500, chunk_overlap=0)
+        # self.text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+
+        # significantly faster than HuggingFace tokenization
+        self.text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            encoding_name="cl100k_base",
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap
+        )
+
 
     @aio.time(Summary("process_plaintext_indexing", "Time spent processing/indexing a plaintext topic."))  # type: ignore
     async def process_message(self, event: pb.FileDataPlaintextMessage) -> None:
@@ -54,15 +74,14 @@ class IndexingService(SingleQueueRabbitMQWorker):
                 print(f"object_id: {object_id}")
                 with await self.storage.download(object_id) as file:
                     with open(file.name, 'r') as f:
-                        # combine all repeated whitespace into single spaces
+
                         text = f.read()
-                        text_processed = re.sub(r"\s+", " ", text)
 
-                        # split the text using our default splitter
-                        docs = self.text_splitter.split_text(text_processed)
-                        total_words = len(text_processed.split())
+                        if text.strip():
 
-                        if total_words > 0:
+                            # split the text using our default splitter
+                            docs = self.text_splitter.split_text(text)
+
                             # construct the metadata dict for each split text
                             metadata = [
                                 {
@@ -74,14 +93,13 @@ class IndexingService(SingleQueueRabbitMQWorker):
                                 }
                             ] * len(docs)
 
-                            await logger.ainfo(f"Loading {len(docs)} documents ({total_words} total words) into the vector store", object_id=object_id)
+                            await logger.ainfo(f"Loading {len(docs)} documents into the vector store", object_id=object_id)
 
                             # load the documents into the vector store asynchronously
-                            #   vector_store.aadd_texts() not yet implemented
                             start = time.time()
-                            self.vector_store.add_texts(docs, metadata)
+                            await self.vector_store.aadd_texts(docs, metadata)
                             end = time.time()
-                            await logger.ainfo(f"Documents loaded in {(end - start):.2f} seconds", object_id=object_id)
+                            await logger.ainfo(f"{len(docs)} documents loaded in {(end - start):.2f} seconds", object_id=object_id)
 
             except Exception as e:
                 await logger.aexception(e, message="exception in processing a plaintext file")
