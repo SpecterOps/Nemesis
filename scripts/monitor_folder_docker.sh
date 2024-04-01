19561c9dc044dd50c0542d30cee4cf422522217b
Will <HarmJ0y@users.noreply.github.com>
2024-04-01 11:30:04 -0700

    Update monitor_folder_docker.sh script
    - Update script with more general Docker host addressing

--- scripts/monitor_folder_docker.sh
+++ scripts/monitor_folder_docker.sh
@@ -6,11 +6,10 @@
 if ! test -f submit_to_nemesis.yaml; then
     # first download the submit config if it doesn't exist
     wget https://raw.githubusercontent.com/SpecterOps/Nemesis/e8a75a5adac1ca93d3ddc5f144306071c302064c/cmd/enrichment/enrichment/cli/submit_to_nemesis/submit_to_nemesis.yaml
+    sed -i 's|http://127.0.0.1:8080/api/|https://host.docker.internal:8080/api/|g' submit_to_nemesis.yaml
 fi
 
-echo -e "\nEdit './submit_to_nemesis.yaml' to reflect your Nemesis instance."
-echo -e "      NOTE: localhost/127.0.0.1 will not work for the Nemesis URL within the container."
-echo -e "            You must use a proper IP/hostname resolvable from the Docker container.\n"
-echo -e "Files copied into $PWD/submit/ will be submitted to Nemesis.\n"
+echo -e "\nEdit './submit_to_nemesis.yaml' to reflect your Nemesis instance and then restart this script!"
+echo -e "\nFiles copied into $PWD/submit/ will be submitted to Nemesis.\n"
 
-docker run -it -v "$PWD/submit_to_nemesis.yaml":/config.yaml -v "$PWD/submit/":/submit/ specterops/nemesis-submit
+docker run -it -v "$PWD/submit_to_nemesis.yaml":/config.yaml -v "$PWD/submit/":/submit/ --add-host=host.docker.internal:host-gateway -t specterops/nemesis-submit
